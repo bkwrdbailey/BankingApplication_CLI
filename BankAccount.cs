0b3ee6bc1f8ef9a11cb9751cbcbb04fc4957a8de
Bailey Barton <baileybarton77@yahoo.com>
2021-10-31 02:52:37 -0500

    Initial Commit

---
+++ BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class BankAccount
+{
+	// User Account Properties
+	
+	protected string Username { get; set; }
+	protected string Password { get; set; }
+	protected string Email { get; set; }
+	protected int UserId { get; set; }
+
+	public BankAccount()
+	{
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingApplication_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
